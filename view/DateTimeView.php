c7b791f2332393842e7d64ac6e9a79118f32bcf0
Micael Persson <micael.c.persson@gmail.com>
2015-09-14 17:58:24 +0200

    Added server time to DateTimeView as specified in test case 1.1.

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -4,8 +4,14 @@ class DateTimeView {
 
 
 	public function show() {
-
-		$timeString = 'TODO, Write servertime here...';
+        /*
+        Set timestring to the following date format
+        [Day of week], the [day of month numeric]th of
+        [Month as text] [year 4 digits].
+        The time is [Hour]:[minutes]:[Seconds].
+        Example: "Monday, the 8th of July 2015, The time is 10:59:21"
+         */
+		$timeString = date("l, jS \of F Y,") . " The time is " . date("H:i:s");
 
 		return '<p>' . $timeString . '</p>';
 	}
