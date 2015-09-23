380a7169ef13a4568ff14451a93da5d7f8244ef8
Micael Persson <micael.c.persson@gmail.com>
2015-09-23 10:26:46 +0200

    Possible solution to Error in Test 1.3: Failed login with only username. No input element with id 'LoginView::UserName' and value 'Admin' found.

--- view/LoginView.php
+++ view/LoginView.php
@@ -69,7 +69,7 @@ class LoginView {
 					<p id="' . self::$messageId . '">' . $message . '</p>
 
 					<label for="' . self::$name . '">Username :</label>
-					<input type="text" id="' . self::$name . '" name="' . self::$name . '" value="" />
+					<input type="text" id="' . self::$name . '" name="' . self::$name . '" value="' . $this->getRequestUserName() . '" />
 
 					<label for="' . self::$password . '">Password :</label>
 					<input type="password" id="' . self::$password . '" name="' . self::$password . '" />
