a7e370dc51811f5af2fc4c640dadc3e053f10097
Micael Persson <micael.c.persson@gmail.com>
2015-09-24 11:06:29 +0200

    Reworked the user class a bit, cleaned up some unused code and added comments.

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -14,12 +14,18 @@ class LoginController {
         $this->loginView = $loginView;
     }
 
+    /**
+     * Checks if user wants to login or log out, fullfills that request and presents
+     * the relevant message in the view.
+     * @return voic
+     */
     public function doLogin()    {
 
         if ($this->loginView->userWantsToLogin() && !$this->user->getLoginStatusFromSession()) {
-            $this->user->validateCredentials($this->loginView->getRequestUserName(),
-                                            $this->loginView->getRequestPassword());
+            $this->user->login($this->loginView->getRequestUserName(),
+                               $this->loginView->getRequestPassword());
 
+            // If login was successful show welcome message
             if ($this->user->getLoginStatusFromSession()) {
                 $this->loginView->setMessageLoginSuccess();
             }
@@ -28,6 +34,7 @@ class LoginController {
         if ($this->loginView->userWantsToLogout() && $this->user->getLoginStatusFromSession()) {
             $this->user->logout();
 
+            // If log out was successful show goodbye message
             if (!$this->user->getLoginStatusFromSession()) {
                 $this->loginView->setMessageLogoutSuccess();
             }
--- model/User.php
+++ model/User.php
@@ -14,41 +14,55 @@ class User {
        $this->password = "Password";
     }
 
-    public function validateCredentials($username, $password)
-    {
-        if ($username == $this->username) {
-
-            if ($password == $this->password) {
-
-                $this->isLoggedIn = true;
-                $this->setLoginStatusInSession();
-
-                return true;
-            }
-        }
-        else {
-
+    /**
+     * Checks if the credentials matches the user's
+     * @param  string $username
+     * @param  string $password
+     * @return bool
+     */
+    private function authenticate($username, $password) {
+        if ($username == $this->username && $password == $this->password) {
+            return true;
+        } else {
             return false;
         }
     }
 
-    public function logout()    {
+    /**
+     * Starts the login process and updates logged in state
+     * @param  string $username
+     * @param  string $password
+     * @return void
+     */
+    public function login($username, $password) {
+        if ($this->authenticate($username, $password)) {
+            $this->isLoggedIn = true;
+            $this->setLoginStatusInSession();
+        }
+    }
 
+    /**
+     * Removes and destroys the session to log out the user
+     * @return void
+     */
+    public function logout() {
         session_unset();
         session_destroy();
     }
 
-    public function getLoginStatus() {
-        return $this->isLoggedIn;
-    }
-
+    /**
+     * Retrieves current logged in state from session
+     * @return [type] [description]
+     */
     public function getLoginStatusFromSession() {
-
         return (isset($_SESSION['isLoggedIn'])) ? $_SESSION['isLoggedIn'] : false;
     }
 
+    /**
+     * Sets the logged in state to the same as the local object's
+     * @return void
+     */
     public function setLoginStatusInSession() {
-
         $_SESSION['isLoggedIn'] = $this->isLoggedIn;
     }
 }
